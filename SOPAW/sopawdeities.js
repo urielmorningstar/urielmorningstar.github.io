9a8a14e893f23bfb55ceb647a324359c8538da5d
urielmorningstar <134649484+urielmorningstar@users.noreply.github.com>
2024-09-18 09:53:58 -0500

    Update sopawdeities.js

--- SOPAW/sopawdeities.js
+++ SOPAW/sopawdeities.js
@@ -1,1 +1,13 @@
+var _ = {
+	"searchentries": [
+		{
+			"title": "Aphrodite",
+			"key": "aphrodite",
+			"description": "A deity from hellenism that rules over the domain of love, beauty, physical attraction, and sexuality.",
+			"tags": [ "Deity", "Hellenism" ]
+		}
+	]
+};
 
+if (typeof ENTRY_DUMP === 'undefined') ENTRY_DUMP = _.searchentries;
+else ENTRY_DUMP.push (..._.searchentries);
