e9606a5a4b851a00775759cefd4a55142b8abfaf
urielmorningstar <134649484+urielmorningstar@users.noreply.github.com>
2024-09-18 14:57:56 -0500

    Update sopawholidays.js

--- SOPAW/dictionaries/sopawholidays.js
+++ SOPAW/dictionaries/sopawholidays.js
@@ -11,56 +11,56 @@ var _ = {
 			"title": "Samhain",
 			"key": "samhain",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/samhain"
 		},
 		{
 			"title": "Yule",
 			"key": "yule",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Solstice" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Solstice" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/yule"
 		},
 		{
 			"title": "Imbolc",
 			"key": "imbolc",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/imbolc"
 		},
 		{
 			"title": "Ostara",
 			"key": "ostara eostara",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Equinox" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Equinox" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/ostara"
 		},
 		{
 			"title": "Beltane",
 			"key": "beltane beltain",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/beltane"
 		},
 		{
 			"title": "Litha",
 			"key": "litha",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Solstice" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Solstice" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/litha"
 		},
 		{
 			"title": "Lughnasadh",
 			"key": "lughnasadh lunasa lammas",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/lughnasadh"
 		},
 		{
 			"title": "Mabon",
 			"key": "mabon",
 			"description": "One of the eight sabbats.",
-			"tags": [ "Wheel-of-the-Year", "Sabbat", "Equinox" ]
+			"tags": [ "Wheel-of-the-Year", "Sabbat", "Equinox" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/mabon"
 		}
 	]
