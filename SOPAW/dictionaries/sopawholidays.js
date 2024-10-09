d1f2acabe748466bf586b89bfcbb430302c2e36d
urielmorningstar <134649484+urielmorningstar@users.noreply.github.com>
2024-10-09 14:48:35 -0500

    Update sopawholidays.js

--- SOPAW/dictionaries/sopawholidays.js
+++ SOPAW/dictionaries/sopawholidays.js
@@ -9,57 +9,57 @@ var _ = {
 		},
 		{
 			"title": "Samhain",
-			"key": "samhain",
-			"description": "One of the eight sabbats.",
+			"key": "samhain halloween all hallows eve",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the beginning of winter, the third harvest, and venoration of ancestors.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/samhain"
 		},
 		{
 			"title": "Yule",
-			"key": "yule",
-			"description": "One of the eight sabbats.",
+			"key": "yule christmas",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the winter solstice and survival against the hardships and pains of the cold.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Solstice" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/yule"
 		},
 		{
 			"title": "Imbolc",
-			"key": "imbolc",
-			"description": "One of the eight sabbats.",
+			"key": "imbolc groundhog day",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the beginning of spring and thus the shedding of the cold.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/imbolc"
 		},
 		{
 			"title": "Ostara",
-			"key": "ostara eostara",
-			"description": "One of the eight sabbats.",
+			"key": "ostara eostara easter",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the spring equinox and the newfound growth therein.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Equinox" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/ostara"
 		},
 		{
 			"title": "Beltane",
 			"key": "beltane beltain",
-			"description": "One of the eight sabbats.",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the beginning of summer.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/beltane"
 		},
 		{
 			"title": "Litha",
 			"key": "litha",
-			"description": "One of the eight sabbats.",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the summer solstice and thus the peak of sunlight.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Solstice" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/litha"
 		},
 		{
 			"title": "Lughnasadh",
 			"key": "lughnasadh lunasa lammas",
-			"description": "One of the eight sabbats.",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the beginning of fall and the first harvest.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Quarter-Day" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/lughnasadh"
 		},
 		{
 			"title": "Mabon",
 			"key": "mabon",
-			"description": "One of the eight sabbats.",
+			"description": "One of the eight sabbats of the wheel of the year, celebrating the autumnal equinox and the second harvest.",
 			"tags": [ "Wheel-of-the-Year", "Sabbat", "Equinox" ],
 			"link": "https://sites.google.com/view/paganreference/holidays/sabbats/mabon"
 		}
